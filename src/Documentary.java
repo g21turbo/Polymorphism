eaa1d387c303af9049780487d2e10c21b9f227ca
g21turbo <dg21stew@gmail.com>
2024-03-17 14:21:38 -0700

    Added Documentary class and main method code

---
+++ src/Documentary.java
@@ -0,0 +1,18 @@
+public class Documentary extends Movie{
+
+    public Documentary(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n" .repeat(3),
+                "Informative scene",
+                "Scenes about the Subjects Life",
+                "Scenes about the Subjects work");
+
+
+    }
+
+}
