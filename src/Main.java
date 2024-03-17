ba6b82aabca6c8f9ad0980c14e4a2ae94fb8a3d8
g21turbo <dg21stew@gmail.com>
2024-03-17 14:19:24 -0700

    Added Documentary class and main method code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Movie theMovie = new Adventure("Star Wars");
-        theMovie.watchMovie();
+//        Movie theMovie = Movie.getMovie("Adventure", "Star Wars");
+//        theMovie.watchMovie();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter Type (A for Adventure, C for Comedy, D for Documentary, S for Science Fiction, or Q to quit");
+            String type = scanner.nextLine();
+            if ("Qq".contains(type)) {
+                break;
+            }
+            System.out.println("Enter a Movie Title");
+            String title = scanner.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
+
 
 
     }
