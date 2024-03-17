b459137fbebd3124915cbcbdad2126ed0387815e
g21turbo <dg21stew@gmail.com>
2024-03-17 12:14:10 -0700

    Added two neew classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Movie theMovie = new Adventure("Star Wars");
+        theMovie.watchMovie();
+
+
     }
 }
